5498eaa995532ae07d9fc43981b7c8061c00d53b
arnaud-4d <arnaud.schmitt@4d.com>
2026-01-27 17:16:42 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1502,7 +1502,7 @@ const sidebar = subCategories.map((sub) => ({
   label: sub.titles[locale], // Titre traduit
     link: {
     type: "doc",
-    id: `commands/theme/${sub.titles['en'].replace(/ /g, '_')}_theme`
+    id: `commands/theme/${sub.titles['en'].replace(/[()]/g, '').replace(/ /g, '_')}`
   },
   items: itemsBySubCategory[sub.id], // Items associés
 }
